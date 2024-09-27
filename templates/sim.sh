a00dcb76b528954200c3b76a0cb8b5bf7d70bba3
John Lavelle <git@lavelle.eu>
2024-09-27 13:58:22 +0200

    Added args

--- templates/sim.sh
+++ templates/sim.sh
@@ -3,7 +3,7 @@
 module load singularity
 
 CONTAINER_LOCATION="/gpfs/projects/ehpc29/EDITO/containers/processor_self_contained.sif"
-CONTAINER_ARGS="from-env"
+CONTAINER_ARGS="-B out:/out -B working:/working:rw"
 
 singularity exec \
   "${CONTAINER_LOCATION}" \
