00172bff8ec392db80607bd822b5af8a463a74a5
John Lavelle <git@lavelle.eu>
2024-09-27 13:49:37 +0200

    Update sim.sh

--- templates/sim.sh
+++ templates/sim.sh
@@ -2,7 +2,7 @@
 
 module load singularity
 
-CONTAINER_LOCATION="/gpfs/projects/ehpc29/EDITO/containers/processor.sif"
+CONTAINER_LOCATION="/gpfs/projects/ehpc29/EDITO/containers/processor_self_contained.sif"
 CONTAINER_ARGS="from-env"
 
 singularity exec \
