d6b4227a40ffbe807690732479b203447dc54f89
John Lavelle <git@lavelle.eu>
2024-10-03 12:01:32 +0200

    bind test_data

--- templates/sim.sh
+++ templates/sim.sh
@@ -3,7 +3,7 @@
 module load singularity
 WRK=/gpfs/projects/ehpc29/EDITO/containers
 CONTAINER_LOCATION="/gpfs/projects/ehpc29/EDITO/containers/processor_self_contained.sif"
-CONTAINER_ARGS_PRE="--bind $WRK/working:/working,$WRK/out:/out"
+CONTAINER_ARGS_PRE="--bind $WRK/working:/working,$WRK/out:/out,$WRK/test_data/:/test_data/"
 singularity exec \
   $CONTAINER_ARGS_PRE \
   "${CONTAINER_LOCATION}"
