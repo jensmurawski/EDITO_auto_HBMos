22fe300f9733bdae57c09a1b59f3cea836f90e30
John Lavelle <git@lavelle.eu>
2024-10-03 15:53:19 +0200

    Update sim.sh

--- templates/sim.sh
+++ templates/sim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 module load singularity
-WRK=/gpfs/projects/ehpc29/EDITO/containers
+WRK=/gpfs/scratch/ehpc29/dmet577222/singularity_working
 CONTAINER_LOCATION="/gpfs/projects/ehpc29/EDITO/containers/processor_self_contained.sif"
-CONTAINER_ARGS="-B out:/out -B working:/working:rw -B test_data:/test_data"
+CONTAINER_ARGS="-B $WRK/out:/out -B $WRK/working:/working:rw -B $WRK/test_data:/test_data"
 
 singularity run \
   $CONTAINER_ARGS \
