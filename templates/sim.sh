b124c35c6ee0a6e8f38f32e55e60e03ca999e9cf
John Lavelle <git@lavelle.eu>
2024-10-03 13:06:54 +0200

    Update sim.sh
    Changed singularity exec to singularity run, which is consistent with the working script.
    Modified the binding arguments to match the format in the working script:
    
    Changed --bind to -B
    Updated the binding syntax to source:destination[:options]
    Added the :rw option to the working directory binding
    
    
    Removed the $WRK prefix from the bind paths, assuming the script will be run from the correct directory
    Added output redirection to singularity_log.txt for both stdout and stderr

--- templates/sim.sh
+++ templates/sim.sh
@@ -1,10 +1,9 @@
-# Script 1: Original script
 #!/bin/bash
 module load singularity
 WRK=/gpfs/projects/ehpc29/EDITO/containers
 CONTAINER_LOCATION="/gpfs/projects/ehpc29/EDITO/containers/processor_self_contained.sif"
-CONTAINER_ARGS_PRE="--bind $WRK/working:/working,$WRK/out:/out,$WRK/test_data/:/test_data/"
-singularity exec \
-  $CONTAINER_ARGS_PRE \
-  "${CONTAINER_LOCATION}"
-  
+CONTAINER_ARGS="-B out:/out -B working:/working:rw -B test_data:/test_data"
+
+singularity run \
+  $CONTAINER_ARGS \
+  "${CONTAINER_LOCATION}" > singularity_log.txt 2>&1
