8c283c9daa4a55228e834d2cb43d6845bbb1cdc2
John Lavelle <git@lavelle.eu>
2024-10-03 11:17:58 +0200

    Removed args

--- templates/sim.sh
+++ templates/sim.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 module load singularity
-WRK=/gpfs/projects/ehpc29/EDITO/containers
 CONTAINER_LOCATION="/gpfs/projects/ehpc29/EDITO/containers/processor_self_contained.sif"
-CONTAINER_ARGS_PRE="--bind $WRK/working:/working,$WRK/out:/out"
+CONTAINER_ARGS=""
 singularity exec \
-  $CONTAINER_ARGS_PRE \
-  "${CONTAINER_LOCATION}"
-echo "OK!"
+  "${CONTAINER_LOCATION}" \
+  ${CONTAINER_ARGS}
