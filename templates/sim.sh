538e429f3e2a3f62714fe610e64ca97f238b0ba8
John Lavelle <git@lavelle.eu>
2024-10-03 11:27:29 +0200

    back to binding

--- templates/sim.sh
+++ templates/sim.sh
@@ -1,7 +1,10 @@
+# Script 1: Original script
 #!/bin/bash
 module load singularity
+WRK=/gpfs/projects/ehpc29/EDITO/containers
 CONTAINER_LOCATION="/gpfs/projects/ehpc29/EDITO/containers/processor_self_contained.sif"
-CONTAINER_ARGS=""
+CONTAINER_ARGS_PRE="--bind $WRK/working:/working,$WRK/out:/out"
 singularity exec \
-  "${CONTAINER_LOCATION}" \
-  ${CONTAINER_ARGS}
+  $CONTAINER_ARGS_PRE \
+  "${CONTAINER_LOCATION}"
+  
