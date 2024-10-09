7ac8db5b223f1c3324eacb09b95f9f74f9640208
John Lavelle <git@lavelle.eu>
2024-10-09 15:54:17 +0200

    Update sim.sh to user Vilnis

--- templates/sim.sh
+++ templates/sim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 module load singularity
-WRK=/gpfs/scratch/ehpc29/dmet577222/singularity_working
+WRK=/gpfs/scratch/ehpc29/dmet429936/singularity_working
 CONTAINER_LOCATION="/gpfs/projects/ehpc29/EDITO/containers/processor_self_contained.sif"
 CONTAINER_ARGS="-B $WRK/out:/out -B $WRK/working:/working:rw -B $WRK/test_data:/test_data"
 
